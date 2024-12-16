97098b8da815fd12b3130766370fbe866a3ae371
pimowo <124725348+pimowo@users.noreply.github.com>
2024-12-16 13:27:54 +0100

    Update lcd.ino

--- lcd.ino
+++ lcd.ino
@@ -211,7 +211,7 @@ void loadSettingsFromEEPROM() {
     bikeSettings.batteryCapacity = 10.0;     // Domyślna pojemność baterii
     bikeSettings.daySetting = 0;
     bikeSettings.nightSetting = 0;
-    bikeSettings.dayRearBlink = false;
+    bikeSettings.dayRearBlink = false;h
     bikeSettings.nightRearBlink = false;
     bikeSettings.blinkInterval = 500;
   }
